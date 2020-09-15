8e9679c9de48920f47aa48db4ef4cb6bf24ab238
Eugene <eshereverov@gmail.com>
2020-09-15 09:23:26 +0300

    configure static assets for dev an prod mode in client

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { join } from 'path'
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
@@ -15,6 +17,10 @@ const environment = process.env.NODE_ENV || 'development';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*']
+    }),
     ConfigModule.forRoot({
       envFilePath: `.env.${environment}`,
       isGlobal: true,
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const globalPrefix = 'api';
+  app.enableCors()
   app.setGlobalPrefix(globalPrefix);
+  app.useStaticAssets(join(__dirname, '..', '..', '..', 'uploads'))
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
