5f17359bab22c445090e16f2120d7d69732983d6
Eugene <eshereverov@gmail.com>
2021-02-19 22:13:58 +0200

    update nx to 11v, update angular to 11.2.1v

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
+  projects: [
+    '<rootDir>/apps/client',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/dto',
+    '<rootDir>/libs/ui',
+    '<rootDir>/libs/data',
+  ],
 };
