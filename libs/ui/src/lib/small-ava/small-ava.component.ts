70b6f694cbd4d749739f6841e1494f92217a6cea
Eugene <eshereverov@gmail.com>
2021-02-27 16:03:34 +0200

    fix small-ava component

--- libs/ui/src/lib/small-ava/small-ava.component.ts
+++ libs/ui/src/lib/small-ava/small-ava.component.ts
@@ -2,13 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { SpriteIconEnum } from '@photobook/data';
 
 @Component({
-  selector: 'photobook-small-ava',
+  selector: 'div[photobook-small-ava], span[photobook-small-ava], a[photobook-small-ava]',
   templateUrl: './small-ava.component.html',
   styleUrls: ['./small-ava.component.scss'],
   host: { class: 'photobook-small-ava' },
 })
 export class SmallAvaComponent implements OnInit {
-  @Input() avatar: string;
+  @Input() avatar?: string;
   @Input() userName: string;
   moreIcon = SpriteIconEnum.more;
   userIcon = SpriteIconEnum.user;
