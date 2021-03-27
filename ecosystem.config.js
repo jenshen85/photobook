33b7bef0d7f17d638acc2a7fb647078a9f6b97d2
Eugene <eshereverov@gmail.com>
2021-03-27 21:53:38 +0200

    ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   deploy: {
     production: {
-      key: '~/.ssh/authorized_keys',
+      key: '~/.ssh/phkey.pub',
       user: 'jenshen',
       // user: `${process.env.TARGET_SERVER_USER}`,
       // host: `${process.env.TARGET_SERVER_HOST}`,
