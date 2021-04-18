6611efa5a795b7b02d0170ec0267a7a830f97060
Eugene <eshereverov@gmail.com>
2021-04-18 09:53:32 +0300

    test push to main branch

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './app.module.scss';
 
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import { Help } from './help'
 
 export function App() {
@@ -11,24 +11,29 @@ export function App() {
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/admin">Home</Link>
           </li>
           <li>
-            <Link to="/help">Page 2</Link>
+            <Link to="/admin/help">Page 2</Link>
           </li>
         </ul>
       </div>
-      <Route path="/" exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route path="/help" exact
-        render={() => <Help/>}
-      />
+      <Switch>
+        <Route path="/admin" exact
+          render={() => (
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          )}
+        />
+        <Route path="/admin/help" exact
+          render={() => <Help/>}
+        />
+        <Route exact path="/">
+          <Redirect to="/admin" />
+        </Route>
+      </Switch>
       {/* END: routes */}
     </div>
   );
